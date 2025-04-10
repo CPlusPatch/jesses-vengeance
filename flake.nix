5c994cbd3849e33d38d7e1a49a96f3385ba9b912
Jesse Wierzbinski <contact@cpluspatch.com>
2025-04-10 23:29:03 +0200

    fix: :bug: Fix Nix flake packaging

--- flake.nix
+++ flake.nix
@@ -2,24 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
@@ -28,75 +10,39 @@
   outputs = {
     self,
     nixpkgs,
-    pyproject-nix,
-    uv2nix,
-    pyproject-build-systems,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
-        config = {
-          permittedInsecurePackages = [
-            "olm-3.2.16"
-          ];
-        };
       };
-      inherit (nixpkgs) lib;
-
-      # Use Python 3.12 from nixpkgs
-      python = pkgs.python312;
-
-      workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
-
-      overlay = workspace.mkPyprojectOverlay {
-        # Prefer prebuilt binary wheels as a package source.
-        # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
-        # Binary wheels are more likely to, but may still require overrides for library dependencies.
-        sourcePreference = "wheel";
-      };
-
-      pyprojectOverrides = import ./overrides.nix;
-
-      # Construct package set
-      pythonSet =
-        # Use base package set from pyproject.nix builders
-        (pkgs.callPackage pyproject-nix.build.packages {
-          inherit python;
-        })
-        .overrideScope
-        (
-          lib.composeManyExtensions [
-            pyproject-build-systems.overlays.default
-            overlay
-            pyprojectOverrides
-          ]
-        );
-
-      # Add metadata attributes to the virtual environment.
-      addMeta = drv:
-        drv.overrideAttrs (old: {
-          # Pass through tests from our package into the virtualenv so they can be discovered externally.
-          passthru = lib.recursiveUpdate (old.passthru or {}) {
-            inherit (pythonSet.testing.passthru) tests;
-          };
-
-          # Set meta.mainProgram for commands like `nix run`.
-          # https://nixos.org/manual/nixpkgs/stable/#var-meta-mainProgram
-          meta =
-            (old.meta or {})
-            // {
-              mainProgram = "bitchbot";
-              description = "fuck you";
-              homepage = "https://github.com/CPlusPatch/jesses-vengeance";
-              license = lib.licenses.agpl3Only;
-            };
-
-          nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.python312Packages.python-olm];
-        });
+      pnpm = pkgs.pnpm_9;
     in {
       packages = rec {
-        bitchbot = addMeta (pythonSet.mkVirtualEnv "bitchbot-env" workspace.deps.default);
+        bitchbot = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "bitchbot";
+          version = "0.0.1";
+
+          src = ./.;
+
+          pnpmDeps = pnpm.fetchDeps {
+            inherit (finalAttrs) pname version src;
+            hash = "sha256-QUudx5A9249NYnjDx19fNysR7usOSmkfjk076RzmrZM=";
+          };
+
+          nativeBuildInputs = [
+            pnpm
+            pkgs.bun
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r . $out/
+            echo '#!${pkgs.bash}/bin/bash' > $out/bin/bitchbot
+            echo '${pkgs.bun}/bin/bun run index.ts' >> $out/bin/bitchbot
+            chmod +x $out/bin/bitchbot
+          '';
+        });
         default = bitchbot;
       };
 
@@ -117,8 +63,8 @@
           ...
         }: let
           cfg = config.services.bitchbot;
-          configFile = configFormat.generate "config.json" cfg.config;
-          configFormat = pkgs.formats.json {};
+          configFile = configFormat.generate "config.toml" cfg.config;
+          configFormat = pkgs.formats.toml {};
 
           inherit (lib.options) mkOption;
           inherit (lib.modules) mkIf;
@@ -140,35 +86,7 @@
               type = with lib.types;
                 submodule {
                   freeformType = configFormat.type;
-                  options = {
-                    # This is a reduced set of popular options and defaults
-                    # Do not add every available option here, they can be specified
-                    # by the user at their own discretion. This is a freeform type!
-
-                    homeserver_url = mkOption {
-                      type = types.str;
-                      description = "The homeserver URL";
-                      default = "https://cpluspatch.dev";
-                    };
-
-                    user_id = mkOption {
-                      type = types.str;
-                      description = "The user ID that the bot will use to login to matrix";
-                      default = "@bitchbot:cpluspatch.dev";
-                    };
-
-                    store_path = mkOption {
-                      type = types.str;
-                      description = "Path to the store for the bot";
-                      default = "./store";
-                    };
-
-                    wife_id = mkOption {
-                      type = types.str;
-                      description = "The user ID of the wife";
-                      default = "@nex:nexy7574.co.uk";
-                    };
-                  };
+                  options = {};
                 };
               description = "Contents of the config file for the bitchbot service";
               default = {};
@@ -221,81 +139,6 @@
             users.groups.bitchbot = {};
           };
         };
-      };
-
-      devShells = {
-        # From https://pyproject-nix.github.io/uv2nix/usage/hello-world.html
-        uv2nix = let
-          # Create an overlay enabling editable mode for all local dependencies.
-          editableOverlay = workspace.mkEditablePyprojectOverlay {
-            # Use environment variable
-            root = "$REPO_ROOT";
-          };
-
-          # Override previous set with our overrideable overlay.
-          editablePythonSet = pythonSet.overrideScope (
-            lib.composeManyExtensions [
-              editableOverlay
-
-              # Apply fixups for building an editable package of your workspace packages
-              (final: prev: {
-                bitchbot = prev.bitchbot.overrideAttrs (old: {
-                  # It's a good idea to filter the sources going into an editable build
-                  # so the editable package doesn't have to be rebuilt on every change.
-                  src = lib.fileset.toSource {
-                    root = old.src;
-                    fileset = lib.fileset.unions [
-                      (old.src + "/pyproject.toml")
-                      (old.src + "/README.md")
-                    ];
-                  };
-
-                  # Hatchling (our build system) has a dependency on the `editables` package when building editables.
-                  #
-                  # In normal Python flows this dependency is dynamically handled, and doesn't need to be explicitly declared.
-                  # This behaviour is documented in PEP-660.
-                  #
-                  # With Nix the dependency needs to be explicitly declared.
-                  nativeBuildInputs =
-                    old.nativeBuildInputs
-                    ++ final.resolveBuildSystem {
-                      editables = [];
-                    };
-                });
-              })
-            ]
-          );
-
-          # Build virtual environment, with local packages being editable.
-          #
-          # Enable all optional dependencies for development.
-          virtualenv = editablePythonSet.mkVirtualEnv "bitchbot-dev-env" workspace.deps.all;
-        in
-          pkgs.mkShell {
-            packages = [
-              virtualenv
-              pkgs.uv
-            ];
-
-            env = {
-              # Don't create venv using uv
-              UV_NO_SYNC = "1";
-
-              # Force uv to use Python interpreter from venv
-              UV_PYTHON = "${virtualenv}/bin/python";
-
-              # Prevent uv from downloading managed Python's
-              UV_PYTHON_DOWNLOADS = "never";
-            };
-
-            shellHook = ''
-              # Undo dependency propagation by nixpkgs.
-              unset PYTHONPATH
-
-              # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
-              export REPO_ROOT=$(git rev-parse --show-toplevel)
-            '';
-          };
       };
     });
 }
