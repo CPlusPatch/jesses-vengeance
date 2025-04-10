1afd97c2ab632a1a702d6d4836b16c2f161a48cb
Jesse Wierzbinski <contact@cpluspatch.com>
2025-04-11 01:39:21 +0200

    fix: :bug: Fix missing Redis config block crashing program

--- src/config.ts
+++ src/config.ts
@@ -2,35 +2,35 @@ import { env } from "bun";
 import { watchConfig } from "c12";
 
 interface Config {
-    login: {
-        homeserver: string;
-        username: string;
-        store_path: string;
-    };
-    monitoring: {
-        health_check_uri?: string;
-    };
-    redis: {
-        url?: string;
-    };
-    commands: {
-        prefix: string;
-    };
-    responses: {
-        cooldown: number;
-    };
-    encryption: {
-        store_path: string;
-    };
-    users: {
-        wife: string;
-        admin: string[];
-        banned: string[];
-    };
+	login: {
+		homeserver: string;
+		username: string;
+		store_path: string;
+	};
+	monitoring: {
+		health_check_uri?: string;
+	};
+	redis?: {
+		url?: string;
+	};
+	commands: {
+		prefix: string;
+	};
+	responses: {
+		cooldown: number;
+	};
+	encryption: {
+		store_path: string;
+	};
+	users: {
+		wife: string;
+		admin: string[];
+		banned: string[];
+	};
 }
 
 export const config = (
-    await watchConfig<Config>({
-        configFile: env.CONFIG_FILE || "config.toml",
-    })
+	await watchConfig<Config>({
+		configFile: env.CONFIG_FILE || "config.toml",
+	})
 ).config;
