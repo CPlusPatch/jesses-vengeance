2f01c0203c9047c560c5c069cc7d40f2100efd4a
Jesse Wierzbinski <contact@cpluspatch.com>
2025-04-20 17:16:21 +0200

    fix: :bug: Specify relative path when searching for credentials file

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ import {
 } from "./commands.ts";
 import { config } from "./config.ts";
 
-const credentialsFile = file("credentials.json");
+const credentialsFile = file("./credentials.json");
 
 export class Bot {
     public client!: MatrixClient;
